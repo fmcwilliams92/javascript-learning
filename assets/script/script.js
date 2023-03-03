a138ee018f304e24fc44bd9d9ed3bbd062dc9b0a
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-02 23:02:02 -0600

    more lessons for js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,9 @@
+// inner.html
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("demo").innerHTML = "This is now the new content of the paragraph";
 }
 
-// myFunction();
+// document.write
+function clickIt() {
+    document.write(5+6)
+}
