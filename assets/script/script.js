3decd40911e97042596cafab811f64d96704262e
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-02 23:36:15 -0600

    testing more functions changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,10 @@
-// inner.html
+// this is for inner.html 
 function myFunction() {
     document.getElementById("demo").innerHTML = "This is now the new content of the paragraph";
+    console.log("this button has been clicked");
 }
 
 // document.write
-function clickIt() {
-    document.write(5+6)
-}
+// function clickIt() {
+//     document.write(5+6)
+// }
