a637e3815d27cb8cf9f10c0f47921b1662b4180c
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-04 17:29:39 -0600

    working on creating new branches to seperate out my training modules

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,6 @@
-// this is a let statement
-let lastName, lastname;
-lastName = "john";
-lastname = "google";
-console.log(lastName, lastname);
-// better understanding how the console log works and where to insert it
-
 // this is for inner.html 
 function myFunction() {
     document.getElementById("demo").innerHTML =
     "This is me working on beutifying the code by proper line breaks";
     console.log("this button has been pressed");
 }
-
-// document.write
-// function clickIt() {
-//     document.write(5+6)
-// }
