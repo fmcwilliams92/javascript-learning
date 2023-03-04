03784f846a100638eb8851e7bbd2f0ea0d4a6cc3
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-04 17:34:26 -0600

    new branch for let

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,3 +4,9 @@ function myFunction() {
     "This is me working on beutifying the code by proper line breaks";
     console.log("this button has been pressed");
 }
+
+// this is a let statement and better understanding how the console log works and where to insert it
+let lastName, lastname;
+lastName = "john";
+lastname = "google";
+console.log(lastName, lastname);
