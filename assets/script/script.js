0fba6070568bef4868d566e9a857de7d71be1b50
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-04 17:41:05 -0600

    working on adding comments to better organize my code

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,3 +4,7 @@ function myFunction() {
     "This is me working on beutifying the code by proper line breaks";
     console.log("this button has been pressed");
 }
+
+// this will be the placeholder for a new funtion
+
+// this is me drilling comments to enrure im mapping out my js as I go
