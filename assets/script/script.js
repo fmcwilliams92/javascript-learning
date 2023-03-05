bd32da286106e342a95c189620e36b0ea1851a4b
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-04 19:47:02 -0600

    working on main branch to drill setting up functions and calling them from html

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,15 @@
+let firstName = "Fred";
+let lastName = "McWilliams";
+let age = 30;
+
 // this is for inner.html 
 function myFunction() {
     document.getElementById("demo").innerHTML =
     "This is me working on beutifying the code by proper line breaks";
     console.log("this button has been pressed");
+}
+
+function returnName() {
+    document.getElementById("userInfo").innerHTML = firstName + " " + lastName + " " + "and I am" + " " + age + " " + "years old"
+    console.log(returnName);
 }
