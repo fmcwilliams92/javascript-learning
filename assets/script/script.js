9471f5c8b1535044123a020a027e9f5cb250f0e2
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-05 07:23:59 -0600

    working with datatypes and there different variations

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,3 +4,42 @@ function myFunction() {
     "This is me working on beutifying the code by proper line breaks";
     console.log("this button has been pressed");
 }
+
+// this is to drill datatype in JS
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+// numbers
+let length = 16;
+let weight = 7.9;
+
+// strings
+let color = "Blue";
+let name = "Fred";
+
+// booleans
+let x = true;
+let y = false;
+
+// object
+const person = {firstName: "Fred", lastName: "McWilliams", age: 30};
+
+// array object
+const cars = ["Ford", "Checy", "Toyota", "tesla"];
+
+// date object
+const date = new Date("2023-03-05");
