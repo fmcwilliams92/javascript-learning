70f7c8b6fe5053db6629ba681a9ff90c7372b3d6
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-05 13:38:09 -0600

    working on functions and how to populate its result into my html

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,9 @@
-// // these are the declared variables at the beginning of my script
-// let firstName = "Fred";
-// let lastName = "McWilliams";
-// let age = 30;
+// these are the declared variables at the beginning of my script
+let firstName = "Fred";
+let lastName = "McWilliams";
+let age = 30;
 
-let x = multiplyFunction(4, 3);
+let x = multiplyFunction(10, 9);
 console.log(x);
 
 function multiplyFunction(a, b) {
@@ -11,14 +11,14 @@ function multiplyFunction(a, b) {
 }
 document.getElementById("functionTest").innerHTML = x;
 
-// // this is for inner.html 
-// function myFunction() {
-//     document.getElementById("demo").innerHTML =
-//     "This is me working on beutifying the code by proper line breaks";
-//     console.log("this button has been pressed");
-// }
+// this is for inner.html 
+function myFunction() {
+    document.getElementById("demo").innerHTML =
+    "This is me working on beutifying the code by proper line breaks";
+    console.log("this button has been pressed");
+}
 
-// function returnName() {
-//     document.getElementById("userInfo").innerHTML = firstName + " " + lastName + " " + "and I am" + " " + age + " " + "years old"
-//     console.log(returnName);
-// }
+function returnName() {
+    document.getElementById("userInfo").innerHTML = firstName + " " + lastName + " " + "and I am" + " " + age + " " + "years old"
+    console.log(returnName);
+}
