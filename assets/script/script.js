61d0b7c6a1cd7bfa6cc0ca911da807ae15337350
Fred McWilliams <fredmcwilliams92@gmail.com>
2023-03-05 13:28:49 -0600

    working on functions

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,24 @@
-// these are the declared variables at the beginning of my script
-let firstName = "Fred";
-let lastName = "McWilliams";
-let age = 30;
+// // these are the declared variables at the beginning of my script
+// let firstName = "Fred";
+// let lastName = "McWilliams";
+// let age = 30;
 
 let x = multiplyFunction(4, 3);
+console.log(x);
 
 function multiplyFunction(a, b) {
-  return a * b,
-  // document.getElementById("multiplyFunction").innerHTML = "The result is" + " " + x,
-  console.log(multiplyFunction);
+  return a * b
 }
+document.getElementById("functionTest").innerHTML = x;
 
+// // this is for inner.html 
+// function myFunction() {
+//     document.getElementById("demo").innerHTML =
+//     "This is me working on beutifying the code by proper line breaks";
+//     console.log("this button has been pressed");
+// }
 
-// this is for inner.html 
-function myFunction() {
-    document.getElementById("demo").innerHTML =
-    "This is me working on beutifying the code by proper line breaks";
-    console.log("this button has been pressed");
-}
-
-function returnName() {
-    document.getElementById("userInfo").innerHTML = firstName + " " + lastName + " " + "and I am" + " " + age + " " + "years old"
-    console.log(returnName);
-}
+// function returnName() {
+//     document.getElementById("userInfo").innerHTML = firstName + " " + lastName + " " + "and I am" + " " + age + " " + "years old"
+//     console.log(returnName);
+// }
